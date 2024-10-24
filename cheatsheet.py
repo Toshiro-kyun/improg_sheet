fd28214fb20cf8469f64d5d7d38b4c20a0e4cfa3
Toshiro <118628118+Toshiro-kyun@users.noreply.github.com>
2024-10-24 15:39:34 +0200

    Create cheatsheet.py

---
+++ cheatsheet.py
@@ -0,0 +1,13 @@
+# Summary of Programming Code for Imperative Programming Course for BSc Articificial Intelligence
+# Feel free to commit/edit this document
+
+#General Info:
+
+#Lecture code:
+
+#Assignment code:
+
+#Extra:
+
+
+
