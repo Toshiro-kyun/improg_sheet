1bb3d4e7fd788a49ef438e006588f334930469b5
vikranthmoparthy <vikranth.moparthy@gmail.com>
2024-10-24 18:51:10 +0200

    cheatsheet.py

--- cheatsheet.py
+++ cheatsheet.py
@@ -6,7 +6,7 @@
 #Lecture code:
 
 #Assignment code:
-  #Function that determines whether a number is divisble by each of its digit:
+  #Function that determines whether a number is divisble by each of its digits:
     def digit_divisible(number):
     for i in number:
         try:
@@ -18,21 +18,61 @@
         else:
             return True
 
+ #Function determining the number of solutions of a given quadratic equation:
+    import math
+    def number_of_quadratic_solutions(a,b,c):
+      number = 0
+      if b**2-4*a*c == 0:
+          number += 1
+          return(number)
+      elif b**2-4*a*c>0:
+          number +=2
+          return(number)
+      elif b**2-4*a*c<0:
+          return(number)
+      
+#Function that recursively exponentiates a number to a given power:
+  def exponentiation(a,b):
+      if b == 0:
+          return 1
+      else:
+          return a * exponentiation(a, b - 1)
+      
+#Function that recursively computes the factorial of a given number:
+  def factorial(x):
+      if x == 0:
+          return 1
+      else:
+          return x * factorial(x-1)
+      
+#Function that determines if a number is prime:
+  def is_prime(n):
+      if n <= 1:
+          return False
+      for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+              return False
+      return True
+    
+#Function that determines if a number is a nested prime:
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-
- #Function determining the number of solution of a given quadratic equation:
-  import math
-  def number_of_quadratic_solutions(a,b,c):
-    number = 0
-    if b**2-4*a*c == 0:
-        number += 1
-        return(number)
-    elif b**2-4*a*c>0:
-        number +=2
-        return(number)
-    elif b**2-4*a*c<0:
-        return(number)
-
+def nested_prime(number):
+    count = len(str(number)) 
+    i = 0
+    while i != count:
+        if is_prime(number/10**i):
+            i += 1
+        else:
+            return False
+        
+    return True
 #Extra:
 
 
