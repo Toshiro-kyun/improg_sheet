1e29c1a4709db810ddf1dbe14031ab9bf2516e2c
Toshiro-kyun <discordtoshiro@gmail.com>
2025-01-29 20:37:21 +0100

    Add sorting explanation

--- improg_sheet.py
+++ improg_sheet.py
@@ -1625,6 +1625,42 @@ def class_function():
   
   del p1 #-> Deletes p1
 
+"""
+Sorting algorithms explanation:
+"""
+"""
+
+Selection sort:
+- Selection Sort is a comparison-based sorting algorithm. It sorts an array by repeatedly selecting the smallest (or largest) element from the unsorted portion and swapping it with the first unsorted element. This process continues until the entire array is sorted.
+1. First we find the smallest element and swap it with the first element. This way we get the smallest element at its correct position.
+2. Then we find the smallest among remaining elements (or second smallest) and swap it with the second element.
+3. We keep doing this until we get all elements moved to correct position.
+
+"""
+
+"""
+Insertion sort:
+1. We start with second element of the array as first element in the array is assumed to be sorted.
+2. Compare second element with the first element and check if the second element is smaller then swap them.
+3. Move to the third element and compare it with the first two elements and put at its correct position
+4. Repeat until the entire array is sorted.
+"""
+
+"""
+Bubble sort:
+1. We sort the array using multiple passes. After the first pass, the maximum element goes to end (its correct position). Same way, after second pass, the second largest element goes to second last position and so on.
+2. In every pass, we process only those elements that have already not moved to correct position. After k passes, the largest k elements must have been moved to the last k positions.
+3. In a pass, we consider remaining elements and compare all adjacent and swap if larger element is before a smaller element. If we keep doing this, we get the largest (among the remaining elements) at its correct position.
+"""
+
+
+"""
+Merge sort:
+1. Divide:  Divide the list or array recursively into two halves until it can no more be divided. 
+2. Conquer:  Each subarray is sorted individually using the merge sort algorithm. 
+3. Merge:  The sorted subarrays are merged back together in sorted order. The process continues until all elements from both subarrays have been merged. 
+"""
+
 
 """
 Methods on how to solve the multiple choice questions:
